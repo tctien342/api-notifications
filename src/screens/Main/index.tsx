247e2f01ec790c46d6aacfad04ae7dde838285c5
Saintno <tctien342@gmail.com>
2022-05-25 10:02:51 +0700

    [Feat] Sync source

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -3,7 +3,13 @@ import './index.scss';
 function MainScreen() {
   return (
     <div className="w-full h-full flex flex-col p-2">
-      <input className="border rounded-md h-10 shadow shadow-zinc-900" />
+      <div className="w-full flex flex-col">
+        <span className="text-xxs ml-1 font-bold text-black dark:text-white">INPUT URL</span>
+        <input
+          placeholder="https://..."
+          className="w-full border rounded-md h-10 shadow shadow-zinc-200 px-3 mt-1 dark:bg-zinc-900 dark:text-white dark:border-zinc-400 outline-transparent dark:shadow-zinc-700"
+        />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontSize: {
+      xxs: '.5rem',
+    },
     extend: {},
   },
   plugins: [],
